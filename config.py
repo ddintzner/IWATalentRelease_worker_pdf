d67d6b4d55ba61f4ca60d3127eea97e6be1ef4e6
ddintzner <ddintzner@gmail.com>
2019-10-06 23:37:03 -0700

    indent config.py characters

--- config.py
+++ config.py
@@ -3,12 +3,11 @@ import os
 class Config(object):
 	
 	AWS_REGION = 'us-west-2'
-    FLASK_DEBUG = 'false'
-    SOURCE_EMAIL_ADDRESS = 'ddintzner@innoceanusa.com'
+	FLASK_DEBUG = 'false'
+	SOURCE_EMAIL_ADDRESS = 'ddintzner@innoceanusa.com'
 
 	SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')  
 	SQLALCHEMY_TRACK_MODIFICATIONS= False
-
 
 	S3_BUCKET =   os.environ.get('S3_BUCKET')  
 	S3_PUBLIC_BUCKET = os.environ.get('S3_PUBLIC_BUCKET')  
