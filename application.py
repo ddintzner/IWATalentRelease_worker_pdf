33c0c0070758136d4f04e3ca441114ad3993481c
ddintzner <ddintzner@gmail.com>
2019-11-27 13:50:33 -0800

    removed reference to imagePortrait before condition

--- application.py
+++ application.py
@@ -333,12 +333,11 @@ def customer_registered():
             uploaded_files = []
             uploadedimages = []
   
-            #print('images load')
-            imagePhoto = get_image_from_obj(application.config["S3_BUCKET"], release['images']['imagePortrait'] )
+            #imagePhoto = get_image_from_obj(application.config["S3_BUCKET"], release['images']['imagePortrait'] )
 
             #portrait
-            asset = ""
 
+            asset = ""
 
             #if image portrait exists
             if "imagePortrait" in release["images"]:
